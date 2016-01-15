08e1b2f5c635b5e372bae8b58414a6e3bcc7c1e0
AlanFloresAlmeraya <oalan28@hotmail.com>
2016-01-15 13:41:34 -0600

    03-OperacionesBasicas

---
+++ 03-OperacionesBasicas/src/AppOperaciones.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ * @author Flores Almeraya Osvaldo Alan
+ *
+ */
+public class AppOperaciones {
+	/**
+	 * 
+	 * @param args En este metodo se crea la ventana y tambien la hacemos visible 
+	 */
+	public static void main(String[] args) {
+		Ventana vtn =new Ventana();
+		vtn.setVisible(true);
+		
+
+	}
+
+}
---
+++ 03-OperacionesBasicas/src/Operaciones.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @author Flores Almeraya Osvaldo Alan
+ *
+ */
+public class Operaciones {
+/**
+ * 	
+ * @param n1 es el primer numero
+ * @param n2 es el segundo numero
+ * @return el valor de la suma 
+ */
+public int sumar (int n1, int n2){
+	
+	return n1+n2;
+}
+}
