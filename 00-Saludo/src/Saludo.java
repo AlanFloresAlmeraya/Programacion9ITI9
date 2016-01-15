45057c19ef4712fb1af601c7beea80670d543bbe
AlanFloresAlmeraya <oalan28@hotmail.com>
2016-01-15 04:20:32 -0600

    00-Saludo

---
+++ 00-Saludo/src/Saludo.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+/**
+ * 
+ * @author OsvaldoAlanFloresAlmeraya
+ *
+ */
+
+public class Saludo {
+	
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "hola Alan");
+		System.out.println("hola desde consola");
+
+	}
+
+}
