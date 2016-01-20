d18ac9483c4c99c38f6fe09a4f889940558d3609
AlanFloresAlmeraya <oalan28@hotmail.com>
2016-01-20 03:37:04 -0600

    05-Perimetros

---
+++ 05-Perimetros/src/AppPerimetros.java
@@ -0,0 +1,12 @@
+
+public class AppPerimetros {
+
+	public static void main(String[] args) {
+		
+		Perimetros p= new Perimetros();
+		System.out.printf("El perimetro del cuadrado es : %d",
+		p.cuadrado(10));
+
+	}
+
+}
