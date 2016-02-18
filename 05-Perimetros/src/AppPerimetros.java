68489ea5dbe0146ede8e687b69d3fbe181fa47df
AlanFloresAlmeraya <oalan28@hotmail.com>
2016-02-18 13:55:08 -0600

    05-Perimetros

--- 05-Perimetros/src/AppPerimetros.java
+++ 05-Perimetros/src/AppPerimetros.java
@@ -1,12 +1,17 @@
-
+/**
+ * 
+ * @author Flores Almeraya Osvaldo Alan
+ *
+ */
 public class AppPerimetros {
-
+	/**
+	 * 
+	 * @param args es el parametro que tienes para tu metodo main (principal)
+	 * En este se crea un objeto de la clase perimetros y lo muestra el consola
+	 */
 	public static void main(String[] args) {
-		
-		Perimetros p= new Perimetros();
-		System.out.printf("El perimetro del cuadrado es : %d",
-		p.cuadrado(10));
-
+		Perimetros p = new Perimetros();
+		System.out.printf("El perimetro del cuadrado es: %d",
+				p.cuadrado(10));
 	}
-
 }
--- 05-Perimetros/src/Perimetros.java
+++ 05-Perimetros/src/Perimetros.java
@@ -1,8 +1,15 @@
-
+/**
+ * 
+ * @author Flores Almeraya Osvaldo Alan
+ *
+ */
 public class Perimetros {
-
+	/**
+	 * 
+	 * @param lado Obtendra el numero 
+	 * @return Retornara la operacion en este caso la suma de 4 lados 
+	 */
 	public int cuadrado(int lado){
-		return lado + lado + lado + lado;
-		
+		return lado + lado +lado + lado;
 	}
 }
